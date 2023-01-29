66b84d625063b9c0f215801e5aaea2765ec68cee
Dmytro Hnipel <dmitrognipel@gmail.com>
2023-01-29 22:02:32 +0200

    optimize js-code in radiobutton with loop

--- script.js
+++ script.js
@@ -1,38 +1,30 @@
 let body = document.querySelector('body')
 let button = document.createElement('button')
-button.textContent = 'LAUNCH'
+button.textContent = 'Number of squares'
 button.classList.add = 'button'
 body.appendChild(button)
 let globalDiv = document.createElement('div')
 globalDiv.className = 'global'
 body.appendChild(globalDiv)
-//get radioButton
-let input1 = document.createElement('input')
-let input2 = document.createElement('input')
-let input3 = document.createElement('input')
-input1.type = 'radio'
-input2.type = 'radio'
-input3.type = 'radio'
-input1.name = 'button'
-input2.name = 'button'
-input3.name = 'button'
-input1.checked = true
-input1.textContent = 'sfjdksdljfk'
-button.insertAdjacentElement('beforebegin', input1)
-button.insertAdjacentElement('beforebegin', input2)
-button.insertAdjacentElement('beforebegin', input3)
-let para1 = document.createElement('p')
-let para2 = document.createElement('p')
-let para3 = document.createElement('p')
-para1.style.display = 'inline'
-para2.style.display = 'inline'
-para3.style.display = 'inline'
-para1.textContent = 'Black'
-para2.textContent = 'Random color'
-para3.textContent = 'From black to white'
-input1.insertAdjacentElement('afterend', para1)
-input2.insertAdjacentElement('afterend', para2)
-input3.insertAdjacentElement('afterend', para3)
+let parag = document.createElement('p')
+button.insertAdjacentElement('afterend', parag)
+//get radioButtons
+for (let i = 1; i <= 3; i++){
+let input = document.createElement('input')
+input.type = 'radio'
+input.name = 'button'
+button.insertAdjacentElement('beforebegin', input)
+let para = document.createElement('p')
+para.style.display = 'inline'
+input.insertAdjacentElement('beforebegin', para)
+}
+let inputs = document.querySelectorAll('input')
+inputs[0].checked = true
+let paras = document.querySelectorAll('p')
+paras[0].textContent = 'Black brush' 
+paras[1].textContent = 'Random color'
+paras[2].textContent = 'From black to white brush' 
+
 //get initiallyGrid
 function getGrid (x) {
     for (i = 1; i <= x; i++){
@@ -54,61 +46,50 @@ row.appendChild(cell)
 getGrid(16)
 //end of getting grid
 
-mouseOverRandomColor()
+mouseOver()
 
 //getNewGrid
 button.addEventListener('click', function func1(){
     let row = document.getElementsByClassName('row')
-    let dimension = prompt('enter dimension of the grid')
-    if (+dimension - +dimension === 0 && dimension !== null && dimension !== ''){
+    let dimension = prompt('set number of squares per side for the new grid')
+    if (+dimension - +dimension === 0
+         && dimension !== null 
+         && dimension !== ''
+         && +dimension <= 100){
     for (i = 0; i < row.length; i++)
     row[i].style.display = 'none'
     getGrid(dimension)
-    if (input1.checked === true) mouseOver()
-    else if (input2.checked === true) mouseOverRandomColor()
-    else if (input3.checked === true) mouseOverWhiteToBlack()
+    mouseOver()
 }
-if(dimension === '' || +dimension - +dimension !== 0){
+if(dimension === '' || +dimension - +dimension !== 0 || +dimension > 100){
     console.log(dimension)
-alert('You need to enter a number!')
+alert('You need to enter a number less than 101!')
 func1()
 }
 })
 //get effect of mouseOver
 function mouseOver() {
     let cells = document.getElementsByClassName('cell')
-    for (let cell of cells) {
-        cell.addEventListener('mouseover', function(){
-            this.classList.add('mouseover')
-        })
-    }
-    }
-function mouseOverRandomColor() {
-    let cells = document.getElementsByClassName('cell')
+    let r = 255, g = 255, b = 255
     for (let cell of cells) {
         cell.addEventListener('mouseover', function func(){
-            let r = Math.floor(Math.random() * 256),
+if  (inputs[0].checked === true) {
+    this.style.backgroundColor = 'black'
+}
+if (inputs[1].checked === true){
+    let r = Math.floor(Math.random() * 256),
             g = Math.floor(Math.random() * 256),
             b = Math.floor(Math.random() * 256)
             this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
-            cell.removeEventListener('mouseover', func)
-        })
-    }
-    }
-function mouseOverWhiteToBlack() {
-    let cells = document.getElementsByClassName('cell')
-    let r = 255, g = 255, b = 255
-    for (let cell of cells) {
-    cell.addEventListener('mouseover', function func(){
+}
+if (inputs[2].checked === true) {
     this.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
     if (r == 0) r = 255, g = 255, b = 255
     r -= 25.5
     g -= 25.5
     b -= 25.5
-    cell.removeEventListener('mouseover', func)
-        })
-    }
-    }
-//end effect of mouseOver
-    
 
+}
+cell.removeEventListener('mouseover', func)})
+}
+}
